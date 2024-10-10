55135984b94906e567d6c4663761b121576db0d5
juanma1510 <jmgarca4@misena.edu.co>
2024-10-10 12:50:40 -0500

    arreglo back, creacion archivo procfile

--- app.py
+++ app.py
@@ -247,7 +247,6 @@ def generate_graphs_spider():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
     
